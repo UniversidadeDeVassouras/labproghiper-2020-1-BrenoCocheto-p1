d7cfdb8019059edce882004a73ae75b930b1d24e
Breno Cocheto <breno.cocheto@gmail.com>
2020-06-05 19:46:28 -0300

    adicionados comentários no arquivo comments_entity

--- application/model/entity/comment_entity.py
+++ application/model/entity/comment_entity.py
@@ -1,13 +1,18 @@
+# cria classe de comentários
 class Comment:
+    # define parametros
     def __init__(self, text, idMovie):
         self._comment = text
         self._MovieId = idMovie
+    # retorna comentários
 
     def getComment(self):
         return self._comment
+    # define comentários
 
     def setComment(self, text):
         self._comment = text
+    # retorna id do video relacionado ao comentário
 
     def getIdMovie(self):
         return self._MovieId
