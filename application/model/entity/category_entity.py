4cb241beaadc798701f8c49a29812a134b81afbf
Breno Cocheto <breno.cocheto@gmail.com>
2020-06-05 19:42:35 -0300

    adicionados comentários do arquivo category_entity

--- application/model/entity/category_entity.py
+++ application/model/entity/category_entity.py
@@ -1,20 +1,27 @@
 from flask import current_app
 
+# inicia a classe de categoria
+
 
 class Category:
+    # passa parametros
     def __init__(self, id, name, description):
         self._id = id
         self._name = name
         self._description = description
+    # retorna o id
 
     def getId(self):
         return self._id
+    # retorna o nome
 
     def getName(self):
         return self._name
+    # retorna a descrição
 
     def getDescription(self):
         return self._description
+    # retorna os vídeos pelo id da categoria
 
     def getVideosByCategory(self):
         movies = current_app.config["movie"]
